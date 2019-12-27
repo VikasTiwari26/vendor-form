7c306fb2f62ab39c8643056d3bb1bc030d3e2706
DivyanshuIn <divyanshu@thepabloproject.io>
2019-12-27 20:11:50 +0530

    vendor form pet fed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  get_places :"https://wiggly-vendor.herokuapp.com/api/v1/getPlaces",
+  location:"https://wiggly-vendor.herokuapp.com/api/v1/getLocation",
+  addVendor:"https://pet-fed.herokuapp.com/api/addvendor",
+
 };
